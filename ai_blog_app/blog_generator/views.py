5331077ee15dad1b370bacb551384432a53b1571
Aniket Mahakalkar <mahakalkaraniket001@gmail.com>
2025-02-10 19:34:55 +0530

    commit #2

--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -152,7 +152,7 @@ def generated_blog_from_transcription(transcription):
 
 
 # def generated_blog_from_transcription(transcription):
-#     openai.api_key = ""
+#     ope.api_key = ""
     
 #     prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
 
